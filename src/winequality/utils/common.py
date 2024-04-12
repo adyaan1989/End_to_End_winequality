8314d2dc43e4b5d599518bfc266efd34d646a230
Adyaan Singh <adyaan.singh@gmail.com>
2024-04-12 23:26:19 +0530

    model evalution added

--- src/winequality/utils/common.py
+++ src/winequality/utils/common.py
@@ -6,6 +6,9 @@ from ensure import ensure_annotations
 from box import ConfigBox
 from pathlib import Path
 from typing import Any
+import json
+import joblib
+
 
 def read_yaml(yaml_path: Path) -> ConfigBox:
     try:
@@ -29,6 +32,44 @@ def create_directories(path_directories: list, verbose=True):
         if verbose:
             logger.info(f"Created directory at: {path}")
 
+@ensure_annotations
+def save_json(path: Path, data: dict):
+    """
+    save json data
+
+    args:
+        path (Path): path to json file
+        data (dict): data to be saved in json file
+    """
+    with open(path, 'w') as f:
+        json.dump(data, f, indent=4)
+    
+    logger.info(f"json file saved at: {path}")
+
+
+@ensure_annotations
+def load_json(path: Path) -> ConfigBox:
+    """load json files data
+
+    Args:
+        path (Path): path to json file
+
+    Returns:
+        ConfigBox: data as class attributes instead of dict
+    """
+    with open(path) as f:
+        content = json.load(f)
+
+    logger.info(f"json file loaded successfully from the : {path}")
+    return ConfigBox(content)
+
+@ensure_annotations
+def load_bin(path: Path) -> Any:
+    data = joblib.load(path)
+    logger.info(f"Binary file loaded successfully from the : {path}")
+    return data
+
+@ensure_annotations
 def get_size(path: Path) -> str:
     size_in_kb = round(os.path.getsize(path) / 1024)
     return f"{size_in_kb} KB"
